532ee5cddab164151a3fdc609fc9d3b1764d65c6
gabrieldev21 <gabrielp.amorim@live.com>
2024-05-01 19:11:08 -0300

    feat: ajuste admin avançado

--- galeria/views.py
+++ galeria/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 def index(request):
-    fotografia = Fotografia.objects.all()
+    fotografia = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
     return render(request, "galeria/index.html", {"cards": fotografia})
 
 
